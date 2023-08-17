95c78a4b4edd02103d9edb58b2d5351d64776904
gleidsontec <142359199+gleidsontec@users.noreply.github.com>
2023-08-16 21:43:55 -0300

    comite

---
+++ cadastro.php
@@ -0,0 +1,32 @@
+   <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TELA DE CADASTRO</title>
+</head>
+<body>
+    <h1>CADASTRE-SE</h1>
+
+    <form 
+            <label for="iusername">Nome de usuário:</label><br>
+            <input type="text" name="username" id="iusername"><br>
+            <br>
+
+            <label for="iemail">E-mail: </label><br>
+            <input type="email" name="email"><br>
+            <br>
+
+            <label for="ipassword">Senha: </label><br>
+            <input type="password" name="password" id="ipassword"><br>
+            <br>
+     
+            <input type="submit" value="Cadastrar">
+            <br>
+            <br>
+
+            <a href="index.php">Voltar</
+            echo "usuario cadastrado com sucesso" 
+        </form>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aluguel de Estudio</title>
+</head>
+<body>
+    
+<?php 
+   
+
+?>
+     <div class="container">
+        <h1>SEJA BEM VINDO!</h1>
+       
+        <form action="process_loguin.php">
+        <label for="iusername">Nome do Usúario </label>
+        <br>
+        <input type="test" name="username"
+        
+        
+        id="iusername" required><br>
+        <br>
+
+        <label for="ipassword">Senha:</label><br>
+        <input type="password" name="password"
+        
+        id="ipassword" required><br>
+        <br>
+        <input type="submit">
+       
+
+        </form>
+
+        <p>Não tem uma conta? <a href="cadastro.php">Cadastre-se aqui</
+
+        
+       
+    </div>
+</body>
+</html>
